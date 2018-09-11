4a5025c2bf5401a4fcd0e5944ca68914b916dbb1
TrevorSeitz <trevinla@gmail.com>
2018-09-11 18:10:36 -0400

    add information window for location

---
+++ src/components/LocationInfoWindow.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { InfoWindow } from 'react-google-maps'
+
+export class LocationInfoWindow extends Component {
+  render() {
+    const { description, name } = this.props
+
+    return (
+      <InfoWindow onCloseClick={this.props.closeWindow}>
+        <div>
+          <h1>{name}</h1>
+          <p>{description}</p>
+          <span></span>
+        </div>
+      </InfoWindow>
+    );
+  }
+}
+
+export default LocationInfoWindow
