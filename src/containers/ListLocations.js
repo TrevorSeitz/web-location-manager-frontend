95588814bd890ff3e48a9c6505ac640d61773b71
TrevorSeitz <trevinla@gmail.com>
2018-10-02 10:03:51 -0400

    Locations listed alphabetically

--- src/containers/ListLocations.js
+++ src/containers/ListLocations.js
@@ -15,6 +15,20 @@ class ListLocations extends Component {
 
   render() {
     debugger;
+    this.props.places.sort(function(a, b) {
+      var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+      var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+
+      // names must be equal
+      return 0;
+    });
+
     return (
       <div>
         <div className="navbar">
